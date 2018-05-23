98b66c881033eab1dd824b9d83c2681b26ca7e7c
Howard <Howard@Rons-Mac-Pro.local>
2018-05-23 14:04:23 -0700

    s2v2

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const helloWorld = new Vue({
     el: '#helloVue',
     data: {
-        title: "HELLO WORLD!!!!!",
-        message: "This is my first Vue template!"
+        title: 'HELLO WORLD!!!!!',
+        message: 'This is my first Vue template!'
+    }
+});
+
+const example = new Vue({
+    el: '#example',
+    data: {
+        title: 'Hello',
+        message: 'This is your daily cat-firmation!',
+        name: "Chewie"
     }
 });
